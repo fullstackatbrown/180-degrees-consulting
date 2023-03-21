40d86725bd598de3fda5ae34cebcac7fc165c908
lukegriley <lukegough@me.com>
2023-03-21 19:34:19 -0400

    deleted gearup code

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import { useState } from "react";
 import "../styles/App.css";
 import Header from "./components/Header";
-import HistoryBox from "./components/HistoryBox";
-import InputBox from "./components/InputBox"
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-  const [history, setHistory] = useState<string[]>([]);
 
   return (
     <div>
-      <Header />
-      <div className="repl">
-        <HistoryBox history={history}/>
-        <hr />
-        <InputBox history={history} setHistory={setHistory}/>
-      </div>
+      <LandingPage></LandingPage>
     </div>
   );
 }
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,7 @@
+import { render } from "react-dom";
+
+function LandingPage() {
+    return (<h1>180 Degrees Consulting at Brown</h1>)
+}   
+
+export default LandingPage;
