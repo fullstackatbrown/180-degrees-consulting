56282d138eef0511712f3561d1da4f147c0ee35f
JayRPhilbrick <jay_philbrick@brown.edu>
2023-04-18 20:13:23 -0400

    added image scaling

--- src/components/LandingPageComponents/PreviousClients.tsx
+++ src/components/LandingPageComponents/PreviousClients.tsx
@@ -3,7 +3,7 @@ import prevClients from './previousClients.png';
 
 function PreviousClients(){
     return <div> <h3>Previous Clients</h3> 
-    <img src={prevClients} alt="previous clients" /> 
+    <img src={prevClients} alt="previous clients" id="prevClients"/> 
     <button >
        Learn About Our Services
     </button>
