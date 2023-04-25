7636b724c6286b3aa69aeb216d2029544fe1d6b8
franchester4 <lukee.choi21@gmail.com>
2023-04-25 18:33:36 -0400

    janky banner, need to work on resize/better text centering

--- src/components/LandingPageComponents/BannerImage.tsx
+++ src/components/LandingPageComponents/BannerImage.tsx
@@ -1,8 +1,12 @@
+import pvd from './pvd.jpeg'
+import './styles/Banner.css'
 
 function BannerImage(){
-  return <div> <h1  id="clubNameText">180 Degrees Consulting <br></br> — Brown University — </h1>
-
-  </div>
+  return ( 
+    <div className='test'>
+      <h1 id="clubNameText">180 Degrees Consulting <br></br> — Brown University — </h1>
+    </div> 
+  )
 }
 
 export default BannerImage;
