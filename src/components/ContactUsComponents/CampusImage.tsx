78b8c5ff68d0ddd0bdab59e2fce24e23b5e17802
risheetha <risheethagb@gmail.com>
2023-10-28 12:48:48 -0400

    added temporary socials

--- src/components/ContactUsComponents/CampusImage.tsx
+++ src/components/ContactUsComponents/CampusImage.tsx
@@ -1,11 +1,9 @@
 import './styles/CampusBanner.css'
 import campus from "./campus.png";
-import whiteBox from "./whiteBox.png";
 
 function CampusImage(){
     return <div className='testing'>
         < img id="campusPic" src={campus}/>
-        < img id="whiteBox" src={whiteBox}/>
     </div>
 }
 
--- src/components/ContactUsComponents/GetInTouch.tsx
+++ src/components/ContactUsComponents/GetInTouch.tsx
@@ -4,6 +4,9 @@ function GetInTouch(){
   return <div className='textBox'>
     <h1 id = "titleText"> Let's Get in Touch! </h1>
     <p id = "desText"> Want to get in Touch? We'd love to hear from you <br/> about any inquiries, feedback, or requests for future <br/> collaboration. </p>
+    <p id = "socials1Text" > Email: <br/> brown@180dc.org </p>
+    <p id = "socials2Text"> Instagram: <br/> @brown180dc </p>
+    <p id = "socials3Text"> Facebook: <br/> https://www.facebook.com/180dcbrown/ </p>
   </div>
 }
 
