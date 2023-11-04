cd78e49de09cfb312a286464cc4c997c7d7afa54
risheetha <risheethagb@gmail.com>
2023-11-04 16:05:59 -0400

    spacing

--- src/components/ContactUsComponents/GetInTouch.tsx
+++ src/components/ContactUsComponents/GetInTouch.tsx
@@ -5,15 +5,15 @@ function GetInTouch(){
   return <div>
     < img style={{ height: "800px", width: '100%', position: "relative", paddingTop: '120px', paddingBottom: '20px' }} src={box}/>
     <p>
-      <a href = "180degrees@brown.edu" style={{ color: '#4A5557', fontSize: "25px", right: -230, top: - 580, position: "relative" }} > 180degrees@brown.edu </a>
+      <a href = "mailto:180degrees@brown.edu" style={{ color: '#4A5557', fontSize: "25px", right: -200, top: - 580, position: "relative" }} > 180degrees@brown.edu </a>
     </p>
     <p>
-        <a href = "https://www.instagram.com/brown180dc/" style={{ color: '#4A5557', right: -165, top: - 500, position: "relative", fontSize: "25px"}}>
+        <a href = "https://www.instagram.com/brown180dc/" style={{ color: '#4A5557', right: -135, top: - 500, position: "relative", fontSize: "25px"}}>
       brown180dc
         </a>
     </p>
     <p>
-      <a href = "https://www.facebook.com/180dcbrown/" style={{ color: '#4A5557', right: -260, top: - 420, position: "relative", fontSize: "25px" }} > Brown 180 DC Facebook Page </a>
+      <a href = "https://www.facebook.com/180dcbrown/" style={{ color: '#4A5557', right: -240, top: - 420, position: "relative", fontSize: "25px" }} > Brown 180 DC Facebook Page </a>
     </p>
   </div>
 }
