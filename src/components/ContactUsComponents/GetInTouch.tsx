dfdd03e2c0794ccad79742fa7ddf3404eed9a1e5
car-losb <77890255+car-losb@users.noreply.github.com>
2023-12-09 18:17:14 -0500

    fixed email link on Contact Us Page

--- src/components/ContactUsComponents/GetInTouch.tsx
+++ src/components/ContactUsComponents/GetInTouch.tsx
@@ -5,7 +5,7 @@ function GetInTouch(){
   return <div>
     < img style={{ height: "800px", width: '100%', position: "relative", paddingTop: '120px', paddingBottom: '20px' }} src={box}/>
     <p>
-      <a href = "180degrees@brown.edu" style={{ color: '#4A5557', fontSize: "25px", right: -230, top: - 580, position: "relative" }} > 180degrees@brown.edu </a>
+      <a href = "mailto:180degrees@brown.edu" style={{ color: '#4A5557', fontSize: "25px", right: -230, top: - 580, position: "relative" }} > 180degrees@brown.edu </a>
     </p>
     <p>
         <a href = "https://www.instagram.com/brown180dc/" style={{ color: '#4A5557', right: -165, top: - 500, position: "relative", fontSize: "25px"}}>
